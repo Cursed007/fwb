736f5aea993c4eeb6e442c5112c2d27b94778eb7
mukesh22584 <mks22584@gmail.com>
2022-10-28 01:00:24 +0530

    base: Fixup! QS tile reset on reboot
    - After r11 merge

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -36,7 +36,6 @@ import android.os.Looper;
 import android.os.Message;
 import android.os.UserHandle;
 import android.provider.Settings;
-import android.provider.Settings.Secure; 
 import android.util.ArrayMap;
 import android.util.AttributeSet;
 import android.util.Log;
